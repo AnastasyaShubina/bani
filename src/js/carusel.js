d0e0e6b27c81407bd497cf193fb4e01d90438e90
Nikita Tkach @F0RES <i@ntkach.ru>
2023-05-29 01:57:28 +0700

    fix mobile

--- src/js/carusel.js
+++ src/js/carusel.js
@@ -7,7 +7,7 @@ import 'swiper/css/pagination';
 export const carusel = () => {
     new Swiper(".carusel", {
         modules: [Navigation, Pagination],
-        slidesPerView: 4,
+        slidesPerView: window.innerWidth > 768 ? 4 : 1,
         // centeredSlides: true,
         spaceBetween: 30,
         grabCursor: true,
