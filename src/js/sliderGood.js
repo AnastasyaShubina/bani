b5327423afe3ebc4af179877d4b1dcdbc47371d7
Nikita Tkach @F0RES <i@ntkach.ru>
2023-05-28 16:47:20 +0700

    fix slider good

--- src/js/sliderGood.js
+++ src/js/sliderGood.js
@@ -5,23 +5,24 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 export const swiperGood = () => {
-      const swiper = new Swiper(".swiperGood", {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesProgress: true,
-      });
-      new Swiper(".swiperGood2", {
-        modules: [Navigation, Pagination],
-        loop: true,
-        spaceBetween: 10,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-          swiper: swiper,
-        },
-      });
+  const swiper = new Swiper(".swiperGood", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    direction: "vertical",
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  new Swiper(".swiperGood2", {
+    modules: [Navigation, Pagination],
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
 }
